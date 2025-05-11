8e8298d3f400e2ea4aa64e9276d31b7a55ca193e
Irwan fernando <136410166+irwanfernando@users.noreply.github.com>
2025-05-11 21:14:10 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,91 @@
-a = 'Halo'
-b = "Python"
-c = """Ini adalah
-string multi-baris"""
-print(type(a))  # <class 'str'>
-print(type(b))  # <class 'str'>
-print(type(c))  # <class 'str'>
+# cara membuat tuple 
 
-# indexing dan slicing 
-text = "IRWAN"
-print(text[0])     # Output: I
-print(text[-1])    # Output: W
-print(text[0:3])   # Output: IRW
-print(text[:4])    # Output: IRWA
-print(text[::2])   # Output: IWA (loncat 2 karakter)
+t = (1234, 'Hello World')
+t1 = (1, 2, 3)
+t2 = "a", "b", "c"        # Tanpa tanda kurung juga valid
+t3 = ()                   # Tuple kosong
+t4 = (5,)                 # Tuple satu elemen (perlu koma!)
 
-# Operasi penggabungan string
-print('Hello ' + 'IRWAN')
+# membuat tuple singleton 
+satu = ('Isi',)
+dua = "ini adalah tuple?",
 
-# Operasi pengulangan string
-print('HOHO' * 3)
+# cek tipe datanya
+print(type(satu))
+print(type(dua))
 
-# Operasi Pengecekan string
-print('a' in 'data') # menghasilkan boolean (True/False)
+# jika tidak pakai koma
+satu = ('Isi')
+dua = "ini adalah tuple?"
 
-# Operasi panjang string
-print(len('IRWAN'))
+# cek tipe datanya
+print(type(satu))
+print(type(dua))
 
-s = "irwan programming"
-print(s.upper())       # 'IRWAN PROGRAMMING'
-print(s.capitalize())  # 'IRWAN programming'
-print(s.title())       # 'IRWAN Programming'
-print(s.replace("irwan", "java"))  # 'java programming'
-print(s.split())       # ['IRWAN', 'programming']
-print(s.find("gram"))  # 10 
+# imdexing dan slicing tuple
 
-# F-String
-# String format menggunakan F-String
-name = "Irwan"
-age = 23
-print(f"Halo, nama saya {name}, umur saya {age}")
+# membuat tuple
+nama = ('alief', 'hikham', 'pratama')
 
-# Format Method
-# String format dengan format method
-print("Halo, nama saya {}, umur saya {}".format(name, age))
+# mengakses indeks ke 1 dari tuple
+print(nama[1])
 
+# Membuat Tuple
+t = (1, 5, 10, 15)
+
+# mengubah isi elemen tuple
+t = (1, 5, 10, 15)
+temp_list = list(t)   # ubah tuple menjadi list
+temp_list[0] = 0      # ubah elemen
+t = tuple(temp_list)  # ubah kembali menjadi tuple
+print(t)              # Output: (0, 5, 10, 15)
+
+# membuat tuple
+angka = (10, 20, 30, 40)
+
+# Memotong tuple
+print(angka[1:3]) 
+print(angka[:2])
+print(angka[2:])
+
+# penggabungan tuple
+print((1, 2) + (3, 4))
+
+# pengulangan tuple
+print(('A',) * 3)
+
+# cek panjang tuple
+data = (1, 2, 4, 5)
+print(len(data))
+
+# cek keanggotaan tuple
+print(3 in data)
+
+# Tuple Bersarang (Nested Tuple)
+t = (1, 2, (3, 4))
+print(t[2][0])  # Output: 3
+
+tuple1 = "aku", "mau", "pinjem"
+tuple2 = "uang", 3, "juta boleh"
+tuple3 = (tuple1, tuple2) # <- nested tuple
+
+print(tuple3)
+
+# tuple berisi list
+t = ([1,2,3,4], True)
+
+print (t)
+
+# mula-mula kita buat tuple seperti ini
+web = 123, "PTI UNDIKMA", "https://fsttundikma.id"
+
+# lalu di-unpacking
+id_web, nama, url = web
+
+# maka sekarang tiga variabel tersebut akan bernilai
+# sesuai yang ada di dalam tuple
+#
+# mari kita cetak
+print(id_web)
+print(nama)
+print(url)
